2580c029527d151be5d7735de8350259005e8d51
Tiago Edgar Rasia <tiago_rasia@hotmail.com>
2023-06-27 17:48:47 -0300

    teste com coleção , stream, sorted ...

---
+++ java-maven/src/main/java/edu/digytal/aulas/OrdenarUmaFrase.java
@@ -0,0 +1,36 @@
+package edu.digytal.aulas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenarUmaFrase {
+    public static void main(String[] args) {
+        String original = "O código estava muito bem escrito.";
+
+        // Separar as palavras por espaços em branco
+        String[] palavras = original.split(" ");
+
+        // Criar uma lista para armazenar as palavras
+        List<String> listaPalavras = new ArrayList<>();
+
+        // Adicionar as palavras na lista
+        Collections.addAll(listaPalavras, palavras);
+
+        // Ordenar a lista em ordem crescente de tamanho das palavras
+        listaPalavras.sort((s1, s2) -> s1.length() - s2.length());
+
+        // Percorrer a lista e exibir as palavras
+        for (String palavra : listaPalavras) {
+            System.out.print(palavra + " ");
+        }
+        System.out.println();
+
+        // OU, todo esse passo a passo em uma linha
+        Arrays.stream("O código estava muito bem escrito.".split(" "))
+                .sorted((s1, s2) -> s1.length() - s2.length())
+                .forEach(palavra -> System.out.print(palavra + " "));
+    }
+
+}
